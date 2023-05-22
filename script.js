1bf00cb416b53d8fc428082833c454f615ef6958
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-22 16:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+    let ele1 =document.getElementsByTagName("input")[0]
+	let ele2 = document.getElementsByTagName("input")[1]
 
+	alert(`Firstname: ${ele1.value} Lastname: ${ele2.value}`);
+	
 }
